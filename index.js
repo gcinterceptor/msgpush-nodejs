b9e1eac2c48462320e9811ee6acab088c2957012
Emerson Cantalice <emersoncantalicee@gmail.com>
2018-08-23 19:14:19 -0300

    Fixes app base

--- index.js
+++ index.js
@@ -3,41 +3,26 @@ var app = express();
 var metrics = require('node-monitor-ui');
 
 const PORT = process.env.PORT || 3000;
-var USE_GCI = process.env.USE_GCI || 'FALSE';
-var MSG_SIZE = process.env.MSG_SIZE || 12;
-var WINDOW_SIZE = process.env.WINDOW_SIZE || 12;
-var buffer = new Array();
-var msgCount = 0;
+const USE_GCI = process.env.USE_GCI || 'FALSE';
+const MSG_SIZE = process.env.MSG_SIZE || 1024;
+const WINDOW_SIZE = process.env.WINDOW_SIZE || 0;
+const buffer = new Array();
+const msgCount = 0;
 
-forceGC();
+metrics.init(3001);
+
+app.listen(PORT, function () {
+    console.log('App listening on port ' + PORT + '!');
+});
+
 app.get('/', function (req, res) {
-
     var byteArray = new Array(MSG_SIZE);
-
     var i = 0;
     for (i; i < MSG_SIZE; i++) {
         byteArray[i] = i;
     }
-
     if (WINDOW_SIZE > 0) {
         buffer[msgCount++ % WINDOW_SIZE] = byteArray;
     }
-
-    console.log(buffer);
-    
-    res.send('hello world' + msgCount);
+    res.status(200).send();
 });
-
-metrics.init(3001);
-
-app.listen(3000, function () {
-    console.log('App listening on port 3000!');
-});
-
-function forceGC() {
-    if (global.gc) {
-        global.gc();
-    } else {
-        console.warn('No GC hook! Start your program as `node --expose-gc file.js`.');
-    }
-};
