dd5cb8572e0ef96e650526bebcb3d6d64307af82
Cantalice <emerson.cantalice@accenture.com>
2018-09-19 16:34:07 -0300

    Create config file experiments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var metrics = require('node-monitor-ui');
 
 const PORT = process.env.PORT || 3000;
 const USE_GCI = process.env.USE_GCI || 'FALSE';
@@ -8,8 +7,6 @@ const MSG_SIZE = process.env.MSG_SIZE || 1024;
 const WINDOW_SIZE = process.env.WINDOW_SIZE || 0;
 const buffer = new Array();
 const msgCount = 0;
-
-metrics.init(3001);
 
 app.listen(PORT, function () {
     console.log('App listening on port ' + PORT + '!');
