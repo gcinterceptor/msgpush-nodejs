4666dd4895fbb49705980fb9285583b53a29ac71
Emerson Cantalice <emersoncantalicee@gmail.com>
2018-10-14 19:33:20 -0700

    Update gcinterceptor.js

--- gcinterceptor.js
+++ gcinterceptor.js
@@ -3,19 +3,28 @@ const v8 = require('v8');
 const gciHeader = 'gci';
 const heapCheckHeader = 'ch';
 
+function getNewSpace(params) {
+    if (params.space_name == "new_space") {
+        return true;
+    };
+}
+
+function getOldSpace(params) {
+    if (params.space_name == "old_space") {
+        return true;
+    };
+}
+
+function getHeapUsage() {
+    return v8.getHeapSpaceStatistics().filter(getNewSpace)[0].space_used_size
+    + "|" + v8.getHeapSpaceStatistics().filter(getOldSpace)[0].space_used_size;
+}
+
 module.exports = function (req, res, next) {
         if (req.headers[gciHeader] != undefined) {
             switch (req.headers[gciHeader]) {
             case heapCheckHeader:
-                res.status(200).send(v8.getHeapSpaceStatistics().filter(function (ns) {
-                    if (ns.space_name == "new_space") {
-                        return true;
-                    };
-                })[0].space_used_size + "|" + v8.getHeapSpaceStatistics().filter(function (os) {
-                    if (os.space_name == "old_space") {
-                        return true;
-                    };
-                })[0].space_used_size);
+                res.status(200).send(getHeapUsage());
             default:
                 global.gc();
             }
